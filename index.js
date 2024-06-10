b8e2a8598c83314c9292c7984cbd0644d166ef94
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-06-10 23:57:22 +0600

    complete code

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt =require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+
 
 // middleware
 app.use(cors());
@@ -21,6 +23,10 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,28 +37,30 @@ async function run() {
     const reviewCollection = client.db("matrimony").collection("reviews");
     const bioCollection = client.db("matrimony").collection("bio");
     const favouritbioCollection = client.db("matrimony").collection("addfavourits");
-  
-   // jwt related api
-   app.post('/jwt', async (req, res) => {
-    const user = req.body;
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-    res.send({ token });
-  })
-   // middlewares 
-   const verifyToken = (req, res, next) => {
-    console.log('inside verify token', req.headers.authorization);
-    if (!req.headers.authorization) {
-      return res.status(401).send({ message: 'unauthorized access' });
-    }
-    const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: 'unauthorized access' })
-      }
-      req.decoded = decoded;
-      next();
+
+    // jwt
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
     })
-  }
+
+       // middlewares 
+       const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
+  
     // use verify admin after verifyToken
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
@@ -113,11 +121,10 @@ app.get('/addfavourits', async(req, res) =>{
 
 app.delete('/addfavourits/:id',async(req,res)=>{
   const id =req.params.id;
-  const query={_id: new ObjectId(id)}
+  const query={_id:new ObjectId(id)}
   const result = await favouritbioCollection.deleteOne(query);
   res.send(result)
 })
-
     app.get('/premium', async(req, res) =>{
         const result = await premiumCollection.find().toArray();
         res.send(result);
@@ -126,6 +133,12 @@ app.delete('/addfavourits/:id',async(req,res)=>{
     app.get('/reviews', async(req, res) =>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    })
+
+    app.post('/reviews',async(req,res)=>{  
+      const reviews = req.body;
+      const result =await reviewCollection.insertOne(reviews);
+      res.send(result);
     })
 
     app.get('/users/admin/:email',  async (req, res) => {
@@ -196,7 +209,7 @@ app.delete('/addfavourits/:id',async(req,res)=>{
         const totalBiodataCount = await bioCollection.countDocuments();
         const maleBiodataCount = await bioCollection.countDocuments({ biodataType: 'Male' });
         const femaleBiodataCount = await bioCollection.countDocuments({ biodataType: 'Female' });
-        const premiumBiodataCount = await bioCollection.countDocuments({ isPremium: true });
+        const premiumBiodataCount = await userCollection.countDocuments({ isPremium: true });
         
         // Assuming there's a revenue field in each biodata document for simplicity
         const totalRevenueData = await bioCollection.aggregate([
@@ -234,5 +247,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Bistro boss is sitting on port ${port}`);
+    console.log(`matromony is sitting on port ${port}`);
 })
