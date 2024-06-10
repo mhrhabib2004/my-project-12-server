648e86e9642825019acd33bab732dd6d0b451107
mhrhabib2004 <mhrhabib2004@gmail.com>
2024-06-10 17:20:24 +0600

    clere some extra code

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const premiumCollection = client.db("matrimony").collection("premium");
     const userCollection = client.db("matrimony").collection("users");
@@ -250,8 +250,8 @@ app.delete('/addfavourits/:id',async(req,res)=>{
    
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
